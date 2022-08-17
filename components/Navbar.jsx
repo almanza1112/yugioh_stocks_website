17977e6560f06754901cbb61f13960b19ea40610
almanza1112 <almanza1112@gmail.com>
2022-08-17 07:18:41 -0400

    navbar functional

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
   return (
-    <div>Navbar</div>
-  )
+    <nav className='fixed w-full h-20 z-[100] p-4 md:p-8 transparent'>
+        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+            <Image src='' alt=''/>
+            <div>
+                <ul className='hidden md:flex items-center'>
+                    <Link href='/'>
+                        <li>Home</li>
+                    </Link>
+                    <Link href='/'>
+                        <li>Watchlist</li>
+                    </Link>
+                    <Link href='/'>
+                        <li>Log In</li>
+                    </Link>
+                    <Link href='/'>
+                        <li className='rounded-lg p-2 bg-neutral-200 text-gray-600'>Sign Up</li>
+                    </Link>
+                </ul>
+            </div>
+
+              <div onClick={handleNav} className='md:hidden'>
+                <AiOutlineMenu size={25}/>
+            </div>
+        </div>
+
+          <div className={nav ? 'md:hidden fixed right-0 top-0 w-full h-screen bg-black/70' : ''}>
+              <div className={nav ? 'fixed right-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-teal-600 p-10 ease-in duration-500' : 'fixed right-[-100%] top-0 p-10 ease-in duration-500'}>
+                <div>
+                    <div className='flex w-full items-center justify-between'>
+                        <Image src='' alt=''/>
+                          <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                            <AiOutlineClose/>
+                        </div>
+                    </div> 
+                    <div className='border-b border-white my-4'>
+                        <p className='w-[85%] md:w-[90%] py-4'>Something will go here</p>
+                    </div>
+                </div>
+
+                <div className=''>
+                    <ul>
+                        <Link href='/'>
+                            <li>Home</li>
+                        </Link>
+                        <Link href='/'>
+                            <li>Watchlist</li>
+                        </Link>
+                        <Link href='/'>
+                            <li>Log In</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='rounded-lg p-2 bg-neutral-200 text-gray-600 w-fit'>Sign Up</li>
+                        </Link>
+                          
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </nav>
+)
 }
 
 export default Navbar
