f16d480935f9750d138344ec4be6fdb197d66ddd
almanza1112 <almanza1112@gmail.com>
2022-08-17 07:54:14 -0400

    search set

---
+++ components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='w-full h-screen flex items-center justify-center'>
+        <input id='search' placeholder='Search'/>
+    </div>
+  )
+}
+
+export default Main
