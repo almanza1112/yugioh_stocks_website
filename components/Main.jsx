bba44363c4502c90ba0eac4ad1c73b7c53c0d67e
almanza1112 <almanza1112@gmail.com>
2022-09-09 23:22:50 -0400

    design updated

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,10 +4,13 @@ import React from 'react'
 const Main = () => {
   return (
     <div className='w-full flex flex-col'>
+      <div className='w-full bg-indigo-700 pt-20'>
         <div className='max-w-screen-2xl w-full h-full mx-auto flex flex-col'>
-          <p className='text-3xl'>Lowest prices </p>
+          <p className='text-3xl text-gray-100'>Lowest prices </p>
 
+        </div>
       </div>
+      
         
         
     </div>
